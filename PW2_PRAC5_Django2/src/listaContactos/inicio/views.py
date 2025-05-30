3a0b18bf6d9964cedaef99025879a211ec9510be
jtijero <jtijero@unsa.edu.pe>
2025-05-29 23:21:18 -0500

    Agrego una nueva vista para la URL another(modificación de urls.py de listaContactos y views.py de inicio)

--- PW2_PRAC5_Django2/src/listaContactos/inicio/views.py
+++ PW2_PRAC5_Django2/src/listaContactos/inicio/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def myHomeView(*args, **kwargs):
   return HttpResponse('<h1>Hola mundo cruel desde Django</h1>')
+def anotherView(request):
+  return HttpResponse('<h1>Solo otra pagina</h1>')
