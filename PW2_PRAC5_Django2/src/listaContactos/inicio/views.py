88ebf8e1030eac86bde471d940a5e588349fb773
jtijero <jtijero@unsa.edu.pe>
2025-05-29 23:25:38 -0500

    argumento de la funciones de las vistas (modificación de views.py de inicio) [en consola de servidor aparece AnonimusUser]

--- PW2_PRAC5_Django2/src/listaContactos/inicio/views.py
+++ PW2_PRAC5_Django2/src/listaContactos/inicio/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def myHomeView(*args, **kwargs):
-  return HttpResponse('<h1>Hola mundo cruel desde Django</h1>')
+def myHomeView(request, *args, **kwargs):
+  print(args,kwargs)
+  print(request.user)
+  return HttpResponse('<h1>Hola mundo desde Django</h1>')
 def anotherView(request):
   return HttpResponse('<h1>Solo otra pagina</h1>')
