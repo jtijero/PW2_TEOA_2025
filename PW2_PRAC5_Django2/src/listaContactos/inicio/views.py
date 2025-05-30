27052b659f5a9d3639420fcaa646f3df9046e6c8
jtijero <jtijero@unsa.edu.pe>
2025-05-29 23:37:07 -0500

    Plantillas (Templates) de Django [creando mi plantilla basica] ((se modifican inicio/view.py listaContactos/settings.py, se crea dir templates y archivo templates/home.html)

--- PW2_PRAC5_Django2/src/listaContactos/inicio/views.py
+++ PW2_PRAC5_Django2/src/listaContactos/inicio/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 def myHomeView(request, *args, **kwargs):
   print(args,kwargs)
   print(request.user)
-  return HttpResponse('<h1>Hola mundo desde Django</h1>')
+  return render(request,"home.html",{})
 def anotherView(request):
   return HttpResponse('<h1>Solo otra pagina</h1>')
