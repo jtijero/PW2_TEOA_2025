3fb343e747c6a3f00a102330b914f3b125f00906
jtijero <jtijero@unsa.edu.pe>
2025-06-18 16:11:40 -0500

    Django 3: Los filtros pueden ser aplicados a bloques: agregacion de |upper |add:10 y otros

--- PW2_PRAC6_Django3/src/listaContactos/inicio/views.py
+++ PW2_PRAC6_Django3/src/listaContactos/inicio/views.py
@@ -6,7 +6,7 @@ def myHomeView(request, *args, **kwargs):
   myContext ={
     'myText': 'Esto es sobre nosotros',
     'myNumber': 123,
-    'myList':[33,44,55],
+    'myList':[3,44,1155],
     }
   return render(request,"home.html", myContext)
 
