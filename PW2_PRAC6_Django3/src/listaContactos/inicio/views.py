cf47a391713766c62255d3ead30d46899cbfe0a5
jtijero <jtijero@unsa.edu.pe>
2025-06-18 15:05:58 -0500

    Django 3: Tag FOR: modificación de función myHomeView en inicio/views.py y templates/home.html

--- PW2_PRAC6_Django3/src/listaContactos/inicio/views.py
+++ PW2_PRAC6_Django3/src/listaContactos/inicio/views.py
@@ -6,7 +6,8 @@ def myHomeView(request, *args, **kwargs):
   myContext ={
     'myText': 'Esto es sobre nosotros',
     'myNumber': 123,
-  }
+    'myList':[33,44,55],
+    }
   return render(request,"home.html", myContext)
 
 def anotherView(request):
