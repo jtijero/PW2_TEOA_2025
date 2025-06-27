d9d75d4c24483fcb049218787a0a002f30f1324f
jtijero <jtijero@unsa.edu.pe>
2025-06-27 16:35:14 -0500

    Django4: Redireccionando: uso de return redirect('../../' despues de eliminar la persona)

--- PW2_PRAC6_Django3/src/listaContactos/personas/views.py
+++ PW2_PRAC6_Django3/src/listaContactos/personas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Persona
 from .forms import PersonaForm, RawPersonaForm
 
@@ -59,6 +59,7 @@ def personasDeleteView(request, myID):
   if request.method == 'POST':
     print("lo borro")
     obj.delete()
+    return redirect('../../')
   context = {
     'objeto': obj,
     }
