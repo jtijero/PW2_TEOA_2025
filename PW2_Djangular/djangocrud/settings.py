f861c79eef42991b23e84457fab7d557ce65b242
jtijero <jtijero@unsa.edu.pe>
2025-07-10 23:54:23 -0500

    Djangular : Configurar settings.py para aceptar peticiones desde Angular

--- PW2_Djangular/djangocrud/settings.py
+++ PW2_Djangular/djangocrud/settings.py
@@ -51,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",  # Angular dev server
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
