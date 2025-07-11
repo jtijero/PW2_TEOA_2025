5b64f4a40bf14c23bfa22e40b7eeaaf3ab4ff4af
jtijero <jtijero@unsa.edu.pe>
2025-07-10 22:32:33 -0500

    Djangular : modificacion en api/views.py > Se define el MovieViewSet en views.py > generacion de endpoints

--- PW2_Djangular/api/views.py
+++ PW2_Djangular/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import MovieSerializer  # Ojo: debe ser serializers.py (plural)
+from .models import Movie
 
-# Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
