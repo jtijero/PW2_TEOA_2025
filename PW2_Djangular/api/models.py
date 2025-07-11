427ffb44dfed5945cfdc6a4f4b1cca73aff1f3fe
jtijero <jtijero@unsa.edu.pe>
2025-07-10 22:10:34 -0500

    Djangular : modificacion de models.py y ejecucion de migraciones

--- PW2_Djangular/api/models.py
+++ PW2_Djangular/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Modelo Movie
+class Movie(models.Model):
+    title = models.CharField(max_length=32)
+    desc = models.CharField(max_length=256)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
